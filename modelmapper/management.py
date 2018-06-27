31d4323eb860ea9182c228d4dddec51ba25110ce
Sep Dehpour <sep@zepworks.com>
2018-06-27 11:24:12 -0700

    dealing with relative paths

--- modelmapper/management.py
+++ modelmapper/management.py
@@ -21,10 +21,8 @@ def analyze(path):
 
 @cli.command()
 @click.option('--debug', is_flag=True)
-@click.option('--path', default='', help='Max number of files to fetch.')
+@click.argument('path', type=click.Path(exists=True, resolve_path=True))
 def run(path, debug):
-    if not path.startswith('/'):
-        sys.exit('Error: Please enter the full path to the setup toml file, NOT relative path.')
     click.echo(f'Running {path}')
     mapper = Mapper(path, debug=debug)
     mapper.run()
