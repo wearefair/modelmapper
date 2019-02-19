96a09683d766702d7b222ed6801979f2f0619505
Jack Latourette <jackl@fair.com>
2019-02-19 15:28:08 -0800

    up the version number for breaking change

--- modelmapper/__init__.py
+++ modelmapper/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 import sys
 pyversion = float(sys.version[:3])
 if pyversion < 3.6:
@@ -10,4 +10,4 @@ from modelmapper.ui import get_user_choice, get_user_input
 from modelmapper.cleaner import Cleaner
 from modelmapper.initialize import initialize
 from modelmapper.etl import ETL
-from modelmapper.loader import PostgresBulkLoaderMixin, PostgresSnapshotLoaderMixin
+from modelmapper.loader import PostgresBulkLoaderMixin, PostgresSnapshotLoaderMixin, PostgresSignatureLoaderMixin
